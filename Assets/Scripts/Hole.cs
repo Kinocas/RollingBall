15d9bb95a0d129136c8ad508d44305aaa4b74bf9
Kinocas <717koushi717@gmail.com>
2021-11-01 18:14:01 +0900

    ゴールしたら中心に止まるように修正

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -12,6 +12,24 @@ public class Hole : MonoBehaviour
         return isHolding;
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        Vector3 direction = other.gameObject.transform.position - transform.position;
+        direction.Normalize();
+
+        if (other.gameObject.tag == targetTag)
+        {
+            r.velocity *= 0.9f;
+            r.AddForce(direction * -20.0f, ForceMode.Acceleration);
+        }
+        else
+        {
+            r.AddForce(direction * 80.0f, ForceMode.Acceleration);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == targetTag)
